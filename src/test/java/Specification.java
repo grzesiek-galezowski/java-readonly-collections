317346c790674a9531a929549523b3c556cfe30e
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2018-07-02 20:38:39 +0200

    removed unused line

--- src/test/java/Specification.java
+++ src/test/java/Specification.java
@@ -1,16 +1,11 @@
-import com.github.grzesiek_galezowski.collections.readonly.implementation.ReadOnlyCollectionWrapper;
 import org.testng.annotations.Test;
-
-import java.util.ArrayList;
 
 public class Specification {
 
     @Test
     public void aasdasd() {
         //GIVEN
-        ReadOnlyCollectionWrapper<Integer>
-            integerReadOnlyCollectionWrapper
-            = new ReadOnlyCollectionWrapper<>(new ArrayList<>());
+
         //WHEN
 
         //THEN
