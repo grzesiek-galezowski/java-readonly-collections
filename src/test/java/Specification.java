5b28128a40ceb4e53ec7a221b97b1c70068d751e
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2018-07-02 20:36:29 +0200

    Changed annotations to compile only

--- src/test/java/Specification.java
+++ src/test/java/Specification.java
@@ -1,11 +1,16 @@
+import com.github.grzesiek_galezowski.collections.readonly.implementation.ReadOnlyCollectionWrapper;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
 
 public class Specification {
 
     @Test
     public void aasdasd() {
         //GIVEN
-
+        ReadOnlyCollectionWrapper<Integer>
+            integerReadOnlyCollectionWrapper
+            = new ReadOnlyCollectionWrapper<>(new ArrayList<>());
         //WHEN
 
         //THEN
